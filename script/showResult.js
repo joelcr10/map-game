9db52d41a8b09d865a13eb3f5780a1b805af567c
Sreejaya V S <sreejaya.vs@experionglobal.local>
2023-12-14 19:04:10 +0530

    feat: showResult function defined

--- script/showResult.js
+++ script/showResult.js
@@ -1,7 +1,31 @@
 import GlobalVariable from "./globalVariable.js";
 
 const showResult = () =>{
-    console.log("hello");
+    let userName = GlobalVariable.username;
+    
+    if(GlobalVariable.correctAnswer>= GlobalVariable.questionNo/2){
+        
+        swal({
+            title: userName+"'s Score",
+            text: "Congratulations,You have scored more than 50%",
+            icon: "success",
+            button: "Reset",
+        }).then(function(){
+            window.location.href = "./index.html";
+        })
+
+    }else{
+
+        swal({
+            title: userName+"'s Score",
+            text: "Oops,You have scored less than 50%",
+            icon: "error",
+            button: "Reset",
+        }).then(function(){
+            window.location.href = "./index.html";
+        })
+
+    }
 }
 
 export default showResult;
