1dbb54acaf858e76dea27558199aaa3a5672a932
Ananthakrishnan C J <ananthakrishnan.cj@experionglobal.local>
2023-12-14 19:46:39 +0530

    feat:button Functions completed

--- script/buttonFunctions.js
+++ script/buttonFunctions.js
@@ -1,8 +1,20 @@
 import { getQuestion } from "./game.js";
 
 export const enableButton = () =>{
+    let nextBtn = document.getElementById("next-question-btn");
+    nextBtn.disabled = false;
+    nextBtn.style.backgroundColor = "#3B71CA";
+    nextBtn.style.cursor = "pointer";
+    nextBtn.style.pointerEvents = "auto";
+    nextBtn.onclick = function(){
+            getQuestion();
+        }
 }
 
 export const disableButton = () =>{
-   
+    let nextBtn = document.getElementById("next-question-btn");
+    nextBtn.disabled = true;
+    nextBtn.style.backgroundColor = "#D3D3D3";
+    nextBtn.style.cursor = "default";
+    nextBtn.style.pointerEvents = "none";
 }
