920bbced22fd61de30a03da5ffca4ec347564817
Sreejaya V S <sreejaya.vs@experionglobal.local>
2023-12-14 19:10:11 +0530

    fix:correctAnswer changed to score

--- script/showResult.js
+++ script/showResult.js
@@ -3,27 +3,35 @@ import GlobalVariable from "./globalVariable.js";
 const showResult = () =>{
     let userName = GlobalVariable.username;
     
-    if(GlobalVariable.correctAnswer>= GlobalVariable.questionNo/2){
+                     //to check if user scored above 50%
+    if(GlobalVariable.score>= GlobalVariable.questionNo/2){
         
         swal({
-            title: userName+"'s Score",
+            title: userName.charAt(0).toUpperCase()+"'s Score",
             text: "Congratulations,You have scored more than 50%",
             icon: "success",
             button: "Reset",
-        }).then(function(){
-            window.location.href = "./index.html";
+             }
+            )
+        .then(function(){
+            window.location.href = "./index.html";//to redirect to the first page
         })
 
-    }else{
+    }
+    else{             //to check if user scored below 50%
 
-        swal({
-            title: userName+"'s Score",
-            text: "Oops,You have scored less than 50%",
-            icon: "error",
-            button: "Reset",
-        }).then(function(){
-            window.location.href = "./index.html";
-        })
+        swal(
+                {
+                 title: userName.charAt(0).toUpperCase()+"'s Score",
+                 text: "Oops,You have scored less than 50%",
+                 icon: "error",
+                 button: "Reset",
+                }
+            )
+        .then(function(){
+                 window.location.href = "./index.html";//to redirect to the first page
+                        }
+             )
 
     }
 }
